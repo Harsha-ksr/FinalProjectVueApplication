d406651e8064e9d178fc31dae2bc09d15bc272e8
Harsha-ksr <harsha.kshirsagar11@gmail.com>
2023-04-30 15:47:40 -0400

    vue commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+ <h1> {{bookName}} </h1>
+    <h2>{{authorName}} </h2 >
+    <h3> {{yourname}} </h3>
+</div>
+</template>
+
+<script>
+
+export default{ // Exporting Default Object
+    name: 'MyHeader',
+    props:{ 
+
+        authorName:String,
+        bookName:String,
+        yourname:String
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+div{
+    text-align: center;
+    background-color: #FAFA33;
+    font-style: sans-serif;
+    padding: 1em;
+    border: 10px solid #ffb89e;
+}
+
+</style>
---
+++ src/components/SingleBook.vue
@@ -0,0 +1,53 @@
+<template>
+    
+
+    <div  :class="book.published ? 'green': 'red' ">
+    <h1> Place Name : {{book.PlaceName}} </h1>
+    <h2> Country : {{book.Country}} </h2>
+    <h4> About : {{book.Description}} </h4>
+    <h4> {{book.FamousFor}} </h4>
+    <h4> Best Time to Visit : {{book.BestTimeToVisit}} </h4>
+    <h4> Ranked : {{book.Rank}} </h4>
+    <h4> No Of Visitors : {{book.NoOfVisitor}} </h4>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'SingleBook',
+    props:{
+        book: Array
+    }
+}
+</script>
+
+<style scoped>
+
+.green{
+    background-color: green;
+}
+
+h4{
+    margin: 2px;
+}
+
+div{
+    background-color: #ffb89e;
+    color: black;
+    padding: 1em;
+    text-align: center;
+    width: 80%;
+    margin: auto;
+    border: 10px solid #FAFA33;
+    
+
+}
+
+.red{
+background-color: orangered !important;
+
+}
+
+
+</style>
